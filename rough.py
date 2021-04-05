153907aa6ad25150a02e4190c8e756bfc751a3bf
Saad <46227224+Saadmairaj@users.noreply.github.com>
2021-04-05 18:44:48 +0530

    testing purpose only.

--- rough.py
+++ rough.py
@@ -8,12 +8,9 @@ def main():
     terminal = Terminal(padx=5, pady=5)
     terminal.shell = True
     terminal.linebar = True
-    # linebar = LineNumberBar(root, padx=5, width=50)
-    # linebar.text = terminal
-    # linebar.pack(side="left", expand=True, fill='y')
     terminal.pack(side="left")
 
-    tk.Button(root, text='Run', command=lambda: print(terminal.run_command("pip uninstall tkmacosx", 'y'))).pack()
+    tk.Button(root, text='Run', command=lambda: print(terminal.run_command("pip uninstall tensorflow", 'y'))).pack()
 
     root.mainloop()
 
